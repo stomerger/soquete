97b6f8d1300f1756818554444d4f9ee750f7c76b
stomerger <63244668+stomerger@users.noreply.github.com>
2020-04-20 11:57:10 -0400

    Agregar archivos de proyecto.

---
+++ cliente/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
